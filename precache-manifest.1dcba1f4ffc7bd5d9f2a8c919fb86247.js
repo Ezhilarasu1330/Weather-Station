8d6e79836712776a760202fbca9aba2763aff101
Ezhilarasu <ezhil9841@gmail.com>
2020-12-05 21:45:11 +0530

    Updates

---
+++ precache-manifest.1dcba1f4ffc7bd5d9f2a8c919fb86247.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f8b097540cebe98c92c276127925ac",
+    "url": "/Weather-Station/index.html"
+  },
+  {
+    "revision": "9e150933699f9516058a",
+    "url": "/Weather-Station/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "50d6597e96eaf0c55a50",
+    "url": "/Weather-Station/static/css/main.ddcf8ccc.chunk.css"
+  },
+  {
+    "revision": "9e150933699f9516058a",
+    "url": "/Weather-Station/static/js/2.a9cbcb86.chunk.js"
+  },
+  {
+    "revision": "50d6597e96eaf0c55a50",
+    "url": "/Weather-Station/static/js/main.6217ac2a.chunk.js"
+  },
+  {
+    "revision": "a773c70218ca0e0c1122",
+    "url": "/Weather-Station/static/js/runtime~main.13ad7614.js"
+  },
+  {
+    "revision": "f963a166b3279d455437165cb78cce6a",
+    "url": "/Weather-Station/static/media/Clear.f963a166.svg"
+  },
+  {
+    "revision": "a82005aa954c61cba1a50336159d70d8",
+    "url": "/Weather-Station/static/media/Cloud-wind.a82005aa.svg"
+  },
+  {
+    "revision": "a98369fb7ac4ac2d522c92642bb69bc8",
+    "url": "/Weather-Station/static/media/Clouds.a98369fb.svg"
+  },
+  {
+    "revision": "1ca35fb1145fa93cb66fa81fc2b9da72",
+    "url": "/Weather-Station/static/media/Drizzle.1ca35fb1.svg"
+  },
+  {
+    "revision": "e3158b56e90d7c4ae2ac85e0ef7ed4bc",
+    "url": "/Weather-Station/static/media/Moon.e3158b56.svg"
+  },
+  {
+    "revision": "06f78ffe921aff71ae391ae110b95c08",
+    "url": "/Weather-Station/static/media/Night-rainy.06f78ffe.svg"
+  },
+  {
+    "revision": "7d9c9233f8d2a0402a90753501bfd21e",
+    "url": "/Weather-Station/static/media/Night.7d9c9233.svg"
+  },
+  {
+    "revision": "29a0aca0c7b11b9e2e20e203bca66089",
+    "url": "/Weather-Station/static/media/Preview.29a0aca0.svg"
+  },
+  {
+    "revision": "b5e8855f869aeaa037290f87cf347fd8",
+    "url": "/Weather-Station/static/media/Rain.b5e8855f.svg"
+  },
+  {
+    "revision": "104ea80ade6e3fd46928a77892b6d86c",
+    "url": "/Weather-Station/static/media/Snow.104ea80a.svg"
+  },
+  {
+    "revision": "c623741c0b9e188e5b6a93a5e1c0ad9e",
+    "url": "/Weather-Station/static/media/Thunderstorm.c623741c.svg"
+  },
+  {
+    "revision": "cd8d7fceea994a098c78b381e4e9916a",
+    "url": "/Weather-Station/static/media/Tonado.cd8d7fce.svg"
+  }
+]);
